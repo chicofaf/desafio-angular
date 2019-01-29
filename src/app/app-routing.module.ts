039ef84e48e2ea364b73526e9d8481bfec7e4d48
francisco <chicofaf@gmail.com>
2019-01-29 21:48:49 -0200

    pesquisar proposta por cpf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormularioCadastroComponent } from './formulario-cadastro/formulario-cadastro.component';
+import {ListaPropostaComponent} from './lista-proposta/lista-proposta.component';
 
-const routes: Routes = [{path:'formulario', component: FormularioCadastroComponent}];
+const routes: Routes = [{path:'formulario', component: FormularioCadastroComponent}, {path: 'lista', component: ListaPropostaComponent}];
 
 @NgModule({
   exports: [RouterModule],
--- src/app/lista-proposta/lista-proposta.component.ts
+++ src/app/lista-proposta/lista-proposta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-proposta',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPropostaComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
   }
+
+  resultado_pesquisa;
+  
+  ngOnInit() {
+    
+  }
+
+  pesquisarPorCpf = (pesquisa) => {
+    this.http.get(`/api/backend/proposta/cpf?cpf=${pesquisa.value.cpf}`).subscribe(data => {
+      if(data && data[0]) this.resultado_pesquisa = data[0].proposta;
+    });
+  }
+
 
 }
